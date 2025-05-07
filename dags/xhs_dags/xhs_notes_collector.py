6863f56c5e8d748e7d34122e5821c73616f42629
orcinusocar <lampropeltisgetulusnigrita@outlook.com>
2025-05-07 16:33:32 +0800

    使用username匹配设备

--- dags/xhs_dags/xhs_notes_collector.py
+++ dags/xhs_dags/xhs_notes_collector.py
@@ -98,13 +98,16 @@ def collect_xhs_notes(**context) -> None:
                 if context['dag_run'].conf
                 else 2)
     
-    # 获取Appium服务器URL
     # 获取设备列表
     device_info_list = Variable.get("XHS_DEVICE_INFO_LIST", default_var=[], deserialize_json=True)
-    # 获取第一个设备的IP、端口和设备ID
-    device_ip = device_info_list[1].get('device_ip', '42.193.193.179') if device_info_list else '42.193.193.179'
-    device_port = device_info_list[1].get('available_appium_ports', [6010])[0] if device_info_list else 6010
-    device_id = device_info_list[1].get('phone_device_list', ['c2c56d1b0107'])[0] if device_info_list else 'c2c56d1b0107'
+    # 获取指定username的设备信息
+    target_username = "rasberry"  # 设置目标username
+    device_info = next((device for device in device_info_list if device.get('username') == target_username), None)
+    
+    # 如果找不到指定username的设备，使用默认值
+    device_ip = device_info.get('device_ip', '42.193.193.179') if device_info else '42.193.193.179'
+    device_port = device_info.get('available_appium_ports', [6010])[0] if device_info else 6010
+    device_id = device_info.get('phone_device_list', ['c2c56d1b0107'])[0] if device_info else 'c2c56d1b0107'
     appium_server_url = f"http://{device_ip}:{device_port}"
     
     
